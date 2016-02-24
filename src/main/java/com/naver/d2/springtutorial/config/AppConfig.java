b58d059fda8a63fd2b96ceccf250303184d0fc40
최영목 <scroogy@swtest.co.kr>
2016-02-25 05:37:08 +0900

    step03 : AOP

--- src/main/java/com/naver/d2/springtutorial/config/AppConfig.java
+++ src/main/java/com/naver/d2/springtutorial/config/AppConfig.java
@@ -4,8 +4,12 @@
  *******************************************************************************/
 package com.naver.d2.springtutorial.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import com.naver.d2.springtutorial.config.aop.LoggingAspect;
 
 /**
  * ApplicationContext 클래스 
@@ -13,7 +17,13 @@ import org.springframework.context.annotation.Configuration;
  * @author <a href="mailto:scroogy@swtest.co.kr">최영목</a>
  */
 @Configuration
+@EnableAspectJAutoProxy
 @ComponentScan({ "com.naver.d2.springtutorial.dao", "com.naver.d2.springtutorial.service" })
 public class AppConfig {
+    
+    @Bean
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
+    }
 
 }
